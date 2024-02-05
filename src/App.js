96ca0ef4429e84d0ef4cce5c2c366bee99169b2c
luisferlie <luisferlie@gmail.com>
2024-02-05 21:24:30 +0100

    añadido formulario contacto componente con ant design

--- src/App.js
+++ src/App.js
@@ -15,8 +15,13 @@ import Faq from './components/Faq'
 import { Collapse } from 'antd';
 import Pricing from './components/Pricing'
 import { Divider, List, Typography } from 'antd';
-export const TaskContext = createContext(null)
+import ContactForm from './components/ContactForm'
 
+import { LockOutlined } from '@ant-design/icons';
+
+
+
+export const TaskContext = createContext(null)
 //Faq
 const text = `
   A dog is a type of domesticated animal.
@@ -44,6 +49,17 @@ const items = [
 
 
 const App = () => {
+  //form
+  const [form] = Form.useForm();
+  const [clientReady, setClientReady] = useState(false);
+
+  useEffect(() => {
+    setClientReady(true);
+  }, []);
+
+  const onFinish = (values) => {
+    console.log('Finish:', values);
+  };
 
   const onChange = (key) => {
     console.log(key);
@@ -143,12 +159,96 @@ const App = () => {
           dataSource={data}
           renderItem={(item) => (
             <List.Item>
-              <Typography.Text mark>[ITEM]</Typography.Text> {item}
+              <Typography.Text mark>[NUMERO]</Typography.Text> {item}
             </List.Item>
           )}
         />
       </article>
+      <Divider />
+      <h1 className='contact-title'>CONTACTA CON NOSOTROS</h1>
+      <Form style={{width:'40%',margin:'auto'}}
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+          <Divider />
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="text"
+              placeholder="telefono"
+            />
+          </Form.Item>
 
+          <Form.Item
+            name="direccion"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="text"
+              placeholder="direccion"
+            />
+          </Form.Item>
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button ">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
 
     </TaskContext.Provider >
 
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ContactForm() {
+  return (
+
+    <div className='block contactBlock'>
+        <div className="container-fluid">
+            <h2>Contactanos</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum debitis labore quas ducimus ratione vero eum ea consectetur eos tenetur?</p>
+        </div>
+    </div>
+  )
+}
+
+export default ContactForm    
